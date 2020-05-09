5ea4e8ab66c405d497b8e4c158455a8e64a24ab3
Flavio Santos <flavio.santos@pravaler.com.br>
2020-05-09 17:12:05 -0300

    Remover atribuicao a parametros

--- remover_atribuicao_a_parametro/index.php
+++ remover_atribuicao_a_parametro/index.php
@@ -2,26 +2,30 @@
 
 function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidade, int $anosCliente): void
 {
+    $desconto = $descontoInicial;
 
-    if ($descontoInicial > 200) {
+    if ($desconto > 200) {
         return;
     }
 
     if ($ehPremium === true) {
-        $descontoInicial = $descontoInicial * 1.1;
+        $desconto = $desconto * 1.1;
     }
 
     if ($quantidade > 50) {
-        $descontoInicial = $descontoInicial * 1.2;
+        $desconto = $desconto * 1.2;
     }
 
     if ($anosCliente > 3) {
-        $descontoInicial = $descontoInicial * 1.1;
+        $desconto = $desconto * 1.1;
     }
+    
+
 
     echo <<<EOF
 --------------------
-Desconto: $descontoInicial
+Desconto inicial: $descontoInicial
+Desconto: $desconto
 --------------------
 EOF;
 }
