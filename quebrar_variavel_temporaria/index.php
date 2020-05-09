8fdbd19918795c84029474240bc9feab5a26194f
Flavio Santos <flavio.santos@pravaler.com.br>
2020-05-09 16:54:08 -0300

    quebra de variavel temporario

--- quebrar_variavel_temporaria/index.php
+++ quebrar_variavel_temporaria/index.php
@@ -1,13 +1,21 @@
-<?php declare(strict_types=1);
+<?php 
+declare(strict_types=1);
 
 function retangulo(int $altura, int $largura): void
 {
 
-    $temp = 2 * ($altura + $largura);
-    echo "Perímetro: $temp" . PHP_EOL;
+    // $temp = 2 * ($altura + $largura);
+    // echo "Perímetro: $temp" . PHP_EOL;
 
-    $temp = $altura * $largura;
-    echo "Área: $temp" . PHP_EOL;
+    // $temp = $altura * $largura;
+    // echo "Área: $temp" . PHP_EOL;
+
+    $parametro = 2 * ($altura + $largura);
+    $are = $altura * $largura;
+
+    echo "Parametro: $parametro <br>";
+    echo "Area: $are";
+
 }
 
 retangulo(3, 10);
