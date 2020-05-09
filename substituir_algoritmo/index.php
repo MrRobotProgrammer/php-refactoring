d1b96712cadece535db92dec6945603c797ecc0d
Flavio Santos <flavio.santos@pravaler.com.br>
2020-05-09 18:11:21 -0300

    Subistitui Algoritmo

--- substituir_algoritmo/index.php
+++ substituir_algoritmo/index.php
@@ -2,7 +2,7 @@
 
 $aulasDisponiveis = [
     'JavaScript Avançado',
-    'Iniciando com Laravel',
+    'Iniciando javair tarde com Laravel',
     'Técnicas de Git',
     'Java Web'
 ];
@@ -13,28 +13,12 @@ function buscaAulas(string $pesquisa, array $aulasDisponiveis): array
     $encontradas = [];
 
     foreach ($aulasDisponiveis as $aula) {
-        if (preg_match("/{$pesquisa}/", $aula) !== 0) {
+        if (preg_match("/{$pesquisa}/i", $aula) !== 0) {
             array_push($encontradas, $aula);
         }
     }
 
-    foreach ($aulasDisponiveis as $aula) {
-        $pesquisaCapitalizada = ucwords($pesquisa);
-        if (preg_match("/$pesquisaCapitalizada/", $aula) !== 0) {
-            array_push($encontradas, $aula);
-        }
-    }
-
-    foreach ($aulasDisponiveis as $aula) {
-        $pesquisaEmLetraMaiuscula = strtoupper($pesquisa);
-        if (preg_match("/$pesquisaEmLetraMaiuscula/", $aula) !== 0) {
-            array_push($encontradas, $aula);
-        }
-    }
-
-    $unicas_encontradas = array_unique($encontradas);
-
-    return $unicas_encontradas;
+    return $encontradas;
 }
 
-var_dump(buscaAulas('Java', $aulasDisponiveis));
+var_dump(buscaAulas('java', $aulasDisponiveis));
